c2c0f1538b5912c66f90b85d170df57b10ba209f
conx42 <97751032+conx42@users.noreply.github.com>
2023-09-10 08:05:05 +0000

    Basic Clean CanVAs

---
+++ timer-clock-app/src/components/Slice/Slice.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import './slice.css';
+const Slice = ()=>{
+    return <>
+    <div className="aApp">
+        <div className="lSlide"></div>
+        <div className="rSlide"></div>
+    </div>
+    </>;
+}
+export default Slice;
---
+++ timer-clock-app/src/components/pages/home/Home.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Slice from "../../Slice/Slice";
+const Home = ()=>{
+    return <>
+    <div className="App">
+        <Slice />
+    </div>
+    </>;
+}
+export default Home;
