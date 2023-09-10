f376da8d29b88b9ae0ad057054bed7e5403c927e
conx42 <97751032+conx42@users.noreply.github.com>
2023-09-10 13:02:39 +0000

    lines_clock

--- timer-clock-app/src/components/Slice/Lslide/Lslide.jsx
+++ timer-clock-app/src/components/Slice/Lslide/Lslide.jsx
@@ -7,6 +7,12 @@ const Lslide = () => {
             let hh = document.getElementById('hh');
             let mm = document.getElementById('mm');
             let ss = document.getElementById('ss');
+            let sec_dot = document.querySelector('.sec_dot');
+            let min_dot = document.querySelector('.min_dot');
+            let hr_dot = document.querySelector('.hr_dot');
+            let hr = document.getElementById('hr');
+            let mn = document.getElementById('mn');
+            let sc = document.getElementById('sc');
 
             let h = new Date().getHours();
             let m = new Date().getMinutes();
@@ -18,6 +24,13 @@ const Lslide = () => {
             // 60 minutes
             ss.style.strokeDashoffset = 760 - (760 * s) / 60;
             // 60 seconds
+
+            sec_dot.style.transform = `rotateZ(${s * 6}deg)`;
+            // 360 / 60seconds = 6
+            min_dot.style.transform = `rotateZ(${m * 6}deg)`;
+            // 360 / 60minutes = 6
+            hr_dot.style.transform = `rotateZ(${h * 30}deg)`;
+            // 360 / 12hrs = 30
         })
     }, []);
     return <>
@@ -45,6 +58,11 @@ const Lslide = () => {
                             </circle>
                         </svg>
                     </div>
+
+                    <div className="niddles" style={{'--clr2':'#ff2972'}} id='sc'><i></i></div>
+                    <div className="niddles niddle2" style={{'--clr2':'#fee800'}} id='sc'><i></i></div>
+                    <div className="niddles niddles3" style={{'--clr2':'#04fc43'}} id='sc'><i></i></div>
+
                 </div>
             </div>
         </div>
