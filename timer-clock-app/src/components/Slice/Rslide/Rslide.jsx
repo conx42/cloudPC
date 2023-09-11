31a2b48a2f65439b78ddd4ef20ef9986b9c37415
conx42 <97751032+conx42@users.noreply.github.com>
2023-09-11 13:15:11 +0000

    Doneeeeeeeeeee YAHHHHHH

--- timer-clock-app/src/components/Slice/Rslide/Rslide.jsx
+++ timer-clock-app/src/components/Slice/Rslide/Rslide.jsx
@@ -5,10 +5,12 @@ const Rslide = () => {
         const playButton = document.getElementsByClassName("play")[0];
         const lapButton = document.getElementsByClassName("lap")[0];
         const resetButton = document.getElementsByClassName("reset")[0];
+        const clearButton = document.getElementsByClassName("lap-clear-button")[0];
         const second = document.getElementsByClassName("sec")[0];
         const centiSecond = document.getElementsByClassName("msec")[0];
         const minute = document.getElementsByClassName("minute")[0];
         const laps = document.getElementsByClassName("laps")[0];
+        const bg = document.getElementsByClassName("outer-circle")[0];
 
         let isPlay = true;
         let secCounter = 0;
@@ -18,6 +20,7 @@ const Rslide = () => {
         let centiCounter=0;
         let minCounter=0;
         let isReset=true;
+        let lapItem=0;
         
 
         const toggleButton = () => {
@@ -31,6 +34,7 @@ const Rslide = () => {
             console.log("isPlay: "+isPlay);
             if (isPlay && isReset) {
                 playButton.innerHTML = 'Pause';
+                bg.classList.add("animation-bg");
                 sec = setInterval(() => {
                     if(secCounter===60){
                         secCounter=0;
@@ -49,6 +53,7 @@ const Rslide = () => {
                 isPlay = false;
                 isReset = false;
             } else {
+                bg.classList.remove("animation-bg");
                 playButton.innerHTML = 'Play';
                 clearInterval(sec);
                 clearInterval(min);
@@ -59,12 +64,12 @@ const Rslide = () => {
             toggleButton();
         }
         // const playSec = () => {
-        //     setInterval(() => {
-        //         second.innerHTML = sec++;
-        //     }, 1000)
-        // }
-        const reset = () => {
-            isReset=true;
+            //     setInterval(() => {
+                //         second.innerHTML = sec++;
+                //     }, 1000)
+                // }
+                const reset = () => {
+                    isReset=true;
             if(!isPlay){
                 play();
             }
@@ -88,15 +93,25 @@ let coutt=0;
             number.setAttribute('class', "number");
             timeStamp.setAttribute('class', 'time-stamp');
 
+            number.innerText=`#${++lapItem}`;
             timeStamp.innerHTML=`${minCounter}&nbsp;:&nbsp;${secCounter}&nbsp;:&nbsp;${centiCounter}`;
 
             li.append(number, timeStamp);
             laps.append(li);
+            clearButton.classList.remove("display-none");
         }
 
+        const clearAll=()=>{
+            lapItem=0;
+            laps.innerHTML=""; 
+            laps.append(clearButton);
+            clearButton.classList.add("display-none");
+        }
+        
         playButton.addEventListener("click", play);
         resetButton.addEventListener("click", reset);
         lapButton.addEventListener("click", lap);
+        clearButton.addEventListener("click", clearAll);
     }, [])
     return <>
         <div className="Container">
@@ -115,7 +130,7 @@ let coutt=0;
                 <button className='button lap display-none'>Lap</button>
             </div>
             <ul className="laps">
-                <button className='lap-clear-button'>
+                <button className='lap-clear-button display-none'>
                     Clear All
                 </button>
             </ul>
